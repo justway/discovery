290b96bc4d86de797557cb5ee0631c038644fd87
HAYASHIMAC <hayashi@fundley.co.jp>
2015-10-10 13:54:14 +0900

    表示テスト

--- recipetest/config/routes.rb
+++ recipetest/config/routes.rb
@@ -1,9 +1,11 @@
 Recipetest::Application.routes.draw do
+  get "recipe/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'recipe#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
